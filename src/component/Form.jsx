0b2bb4f229f420a292a1b548540a882c220c1457
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-18 14:18:59 +0530

    Changes related to to mark todo  Item as completed

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react'
 import styles from './form.module.css'
 
 function Form({ todoItem, setTodoItem }) {
-    const [todo, setTodo] = useState("");
+    const [todo, setTodo] = useState({ todoName: "", todoDone: false });
 
     const handleSubmitForm = (e) => {
         e.preventDefault();
         setTodoItem([...todoItem, todo]);
-        setTodo("");
+        setTodo({ todoName: "", todoDone: false });
     };
     const addTodoItem = (e) => {
-        setTodo(e.target.value);
+        setTodo({ todoName: e.target.value, todoDone: false });
     };
     return (
         <form className={styles.todoForm} onSubmit={handleSubmitForm}>
             <div className={styles.form_container}>
-                <input className={styles.mordernInput} onChange={addTodoItem} type="text" value={todo} placeholder='Enter the todo item...' />
+                <input className={styles.mordernInput} onChange={addTodoItem} type="text" value={todo.todoName} placeholder='Enter the todo item...' />
                 <button className={styles.mordernButton} type="submit">Add</button>
             </div>
         </form>
--- src/component/TodoItems.jsx
+++ src/component/TodoItems.jsx
@@ -4,14 +4,27 @@ import styles from "./todoItem.module.css";
 function TodoItems({ item, todoItem, setTodoItem }) {
     const handleDeleteItem = (item) => {
         console.log("delete button clicked for ", item);
-        setTodoItem(todoItem.filter((todo) => todo !== item))
+        setTodoItem(todoItem.filter((todo) => todo !== item));
+    };
+
+    const handleItemComplete = (name) => {
+        // console.log("The item clicked is ", item)
+        const newTodoItems = todoItem.map((todo) => todo.todoName === name ? { ...todo, todoDone: !todo.todoDone } : todo);
+        setTodoItem(newTodoItems)
+        console.log(todoItem)
     }
+    const strikeThrough = item.todoDone ? styles.completed : ""
     return (
         <div className={styles.item}>
             <div className={styles.itemName}>
-                {item}
+                <span className={strikeThrough} onClick={() => handleItemComplete(item.todoName)}>{item.todoName}</span>
                 <span>
-                    <button onClick={() => handleDeleteItem(item)} className={styles.deleteButton}>x</button>
+                    <button
+                        onClick={() => handleDeleteItem(item)}
+                        className={styles.deleteButton}
+                    >
+                        x
+                    </button>
                 </span>
             </div>
             <hr className={styles.line} />
