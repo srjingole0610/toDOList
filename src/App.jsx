e36d8841d9def5e509c5932bff6b92909d534132
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-18 10:28:41 +0530

    Added header component and styling to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import TodoForm from "./component/TodoForm"
-
+import Header from "./component/Header"
+import "./App.css"
 function App() {
   return (
     <div>
+      <Header />
       <TodoForm />
     </div>
   )
 }
 
 export default App
+
---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './header.module.css'
+function h1() {
+    return (
+        <div>
+            <h1 className={styles.header}>My TODO List</h1>
+        </div>
+    )
+}
+
+export default h1
