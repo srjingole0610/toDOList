5a95e3677bd46b16f7cae276f8a378c6c14112c8
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-18 12:08:02 +0530

    Added and style delete button

--- src/component/TodoItems.jsx
+++ src/component/TodoItems.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import styles from "./todoItem.module.css"
+import React from "react";
+import styles from "./todoItem.module.css";
 
 function TodoItems({ item }) {
     return (
         <div className={styles.item}>
             <div className={styles.itemName}>
                 {item}
+                <span>
+                    <button className={styles.deleteButton}>x</button>
+                </span>
             </div>
             <hr className={styles.line} />
         </div>
-    )
+    );
 }
 
-export default TodoItems
+export default TodoItems;
